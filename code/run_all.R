df52158309ab5f796edb8dba9513de3d566d87d5
cooperbrueck-spec <cooperbrueck@gmail.com>
2026-04-12 10:33:44 -0600

    Update run_all.R

--- code/run_all.R
+++ code/run_all.R
@@ -1,10 +1,5 @@
 rm(list = ls())
 
-# Ensure project-root working directory when sourced from RStudio
-this_file <- normalizePath(sys.frame(1)$ofile)
-project_root <- dirname(dirname(this_file))
-setwd(project_root)
-
 # WARNING: The NASA POWER weather pull step can take a long time to complete
 # because it requests weather data for many tournament-year combinations.
 
@@ -19,5 +14,6 @@ source("code/Full_tennis_weather_data_set_cleaned_nofeatures.R", echo = TRUE)
 source("code/ranking_feature_contruction.R", echo = TRUE)
 source("code/rankings_merge.R", echo = TRUE)
 source("code/Data_set_visualizations.R", echo = TRUE)
+source("code/build_final_modeling_dataset.R", echo = TRUE)
 
 message("Pipeline complete.")
